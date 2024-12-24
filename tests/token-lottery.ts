fac76f61ab6b697de6b532944b17abd9cae1a1bf
judeVector <ikechukwujudendubuisi@yahoo.com>
2024-12-24 22:40:01 +0400

    Add test for creating of lottery

--- tests/token-lottery.ts
+++ tests/token-lottery.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TokenLottery } from "../target/types/token_lottery";
+import { TOKEN_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
 
 describe("token-lottery", () => {
   // Configure the client to use the local cluster.
@@ -28,5 +29,30 @@ describe("token-lottery", () => {
       wallet.payer,
     ]);
     console.log("Your transaction signature", signature);
+  });
+
+  it("Should Initialize Lottery", async () => {
+    const initLotteryTx = await program.methods
+      .initializeLottery()
+      .accounts({
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .instruction();
+
+    const blockhashWithContext = await provider.connection.getLatestBlockhash();
+
+    const tx = new anchor.web3.Transaction({
+      feePayer: provider.wallet.publicKey,
+      blockhash: blockhashWithContext.blockhash,
+      lastValidBlockHeight: blockhashWithContext.lastValidBlockHeight,
+    }).add(initLotteryTx);
+
+    const signature = await anchor.web3.sendAndConfirmTransaction(
+      provider.connection,
+      tx,
+      [wallet.payer]
+      // { skipPreflight: true }
+    );
+    console.log("Your initLottery transaction signature", signature);
   });
 });
