e290ca4866848127ca0044e37bab2a8f95f6e059
judeVector <ikechukwujudendubuisi@yahoo.com>
2024-12-30 00:43:56 +0400

    (Update): Add logic for revealing winning ticket

--- programs/token-lottery/src/instructions/mod.rs
+++ programs/token-lottery/src/instructions/mod.rs
@@ -2,8 +2,10 @@ pub mod buy_ticket;
 pub mod commit_randomness;
 pub mod initialize_config;
 pub mod initialize_lottery;
+pub mod reveal_winning_ticket;
 
 pub use buy_ticket::*;
 pub use commit_randomness::*;
 pub use initialize_config::*;
 pub use initialize_lottery::*;
+pub use reveal_winning_ticket::*;
---
+++ programs/token-lottery/src/instructions/reveal_winning_ticket.rs
@@ -0,0 +1,57 @@
+use anchor_lang::prelude::*;
+use switchboard_on_demand::RandomnessAccountData;
+
+use crate::{CustomError, TokenLottery};
+
+#[derive(Accounts)]
+pub struct RevealWinningTicket<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"token_lottery".as_ref()],
+        bump
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+
+    /// CHECK: This account is checked by the Switchboard smart contract
+    pub randomness_account: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler_reveal_winning_ticket(ctx: Context<RevealWinningTicket>) -> Result<()> {
+    let clock = Clock::get()?;
+    let token_lottery = &mut ctx.accounts.token_lottery;
+
+    if ctx.accounts.signer.key() != token_lottery.authority {
+        return Err(CustomError::Unauthorized.into());
+    }
+
+    if ctx.accounts.randomness_account.key() != token_lottery.randomness_account {
+        return Err(CustomError::IncorrectRandomnessAccount.into());
+    }
+
+    if clock.slot < token_lottery.end_time {
+        return Err(CustomError::LotteryNotCompleted.into());
+    }
+
+    require!(!token_lottery.winner_claimed, CustomError::WinnerClaimed);
+
+    let randomness_data =
+        RandomnessAccountData::parse(ctx.accounts.randomness_account.data.borrow()).unwrap();
+
+    let reveal_random_value = randomness_data
+        .get_value(&clock)
+        .map_err(|_| CustomError::RandomnessNotResolved)?;
+
+    let winner = reveal_random_value[0] as u64 % token_lottery.total_tickets;
+
+    msg!("Winner chosen: {}", winner);
+
+    token_lottery.winner = winner;
+    token_lottery.winner_claimed = true;
+
+    Ok(())
+}
